aa47976d1dae524c0b2ddfd78b60c70c62122a47
ylStudent <yilingwa@163.com>
2023-10-29 19:36:41 +0800

    delete file

--- CMU15-445/homeworks/q10_genre_counts.sql
+++
@@ -1,5 +0,0 @@
-select  titles.genres, count(titles.primary_title) as title_count
-    from titles
-    where titles.genres is not null and titles.primary_title is not null
-    group by titles.genres
-    order by title_count desc;
